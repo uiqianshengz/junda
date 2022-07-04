d7cbbcad08e6a0885c455243821b3b63263cf9dc
wangsongsen <18803979876@163.com>
2022-07-04 09:03:27 +0800

    前后端分离

--- src/pages/Business/store/actionCreators.js
+++ src/pages/Business/store/actionCreators.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 import * as actionTypes from './actionTypes'
-const estateList=(data)=>({
-    type:actionTypes.ESTATE_LIST,
+const estateList = (data) => ({
+    type: actionTypes.ESTATE_LIST,
     data
 })
-export const getEstateList=()=>{
-    return async(dispatch)=>{
-       let{data:res}=await axios.get('/api/estateList.json')
-    //    console.log(res.data.before);
-       if(res.status!==200)return
-       dispatch(estateList(res.data))
+export const getEstateList = () => {
+    return async (dispatch) => {
+        let { data: res } = await axios.get('http://localhost:3004/estateList')
+        //    console.log(res.data.before);
+        if (res.status !== 200) return
+        dispatch(estateList(res.data))
     }
 }
--- src/pages/Project/store/actionCreators.js
+++ src/pages/Project/store/actionCreators.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 import * as actionTypes from './actionTypes'
-const projectList=(data)=>({
-    type:actionTypes.PROJECT_LIST,
+const projectList = (data) => ({
+    type: actionTypes.PROJECT_LIST,
     data
 })
-export const getProjectList=()=>{
-    return async(dispatch)=>{
-       let{data:res}=await axios.get('/api/projectList.json')
-       console.log(res.data);
-       if(res.status!==200)return
-       dispatch(projectList(res.data))
+export const getProjectList = () => {
+    return async (dispatch) => {
+        let { data: res } = await axios.get('http://localhost:3004/projectList')
+        console.log(res.data);
+        if (res.status !== 200) return
+        dispatch(projectList(res.data))
     }
 }
